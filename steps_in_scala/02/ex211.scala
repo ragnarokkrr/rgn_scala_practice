6bfc8c83151b46cd5140b493675066723636f7b2
Nilseu Padilha <ragnarokkrr.blog@gmail.com>
2012-03-13 23:01:21 -0300

    steps in scala
    Signed-off-by: Nilseu Padilha <ragnarokkrr.blog@gmail.com>

---
+++ steps_in_scala/02/ex211.scala
@@ -0,0 +1,28 @@
+var max	= 0
+var EOF	= false
+var firstTime = true
+var x = 0
+
+do {
+	print ("gimme a number: ");
+	
+	try{
+		x = readInt()
+		println(x)
+	}catch{
+		case eof: java.io.IOException => EOF = !EOF
+		case numFormat: java.lang.NumberFormatException => EOF = !EOF
+	}
+
+	
+	if (!EOF){
+		if(firstTime){
+			max = x
+			firstTime = !firstTime
+		}
+		if (x > max)
+			max = x
+	}
+} while(!EOF)
+println(!EOF)
+	
