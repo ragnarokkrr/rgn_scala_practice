10301bcaa24a5a970025bea966cdec2bcbe7ad4c
Nilseu Padilha <ragnarokkrr.blog@gmail.com>
2012-03-15 23:25:55 -0300

    arranging src folders
    Signed-off-by: Nilseu Padilha <ragnarokkrr.blog@gmail.com>

---
+++ Scala Studies.mm
@@ -0,0 +1,65 @@
+<map version="0.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Scala Studies" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1331860080545"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="12" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="20"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="12"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node TEXT="Steps in Scala" POSITION="right" ID="ID_1387190755" CREATED="1331859054685" MODIFIED="1331860107071">
+<edge COLOR="#ff0000" WIDTH="3"/>
+</node>
+<node TEXT="Actors in Scala" POSITION="right" ID="ID_1519688693" CREATED="1331860107961" MODIFIED="1331860112038">
+<edge COLOR="#0000ff" WIDTH="3"/>
+</node>
+<node TEXT="A Tour of Scala" POSITION="left" ID="ID_5105349" CREATED="1331861881232" MODIFIED="1331861881235">
+<edge COLOR="#ff00ff" WIDTH="3"/>
+<node TEXT="http://www.scala-lang.org/node/104" ID="ID_1424289045" CREATED="1331861849322" MODIFIED="1331861885991" LINK="http://www.scala-lang.org/node/104"/>
+</node>
+</node>
+</map>
