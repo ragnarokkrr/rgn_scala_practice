e6d6550cbd1f89265ff6d19aa2fc1f10583b1cae
Nilseu Padilha <ragnarokkrr.blog@gmail.com>
2013-05-24 04:56:32 -0300

    p04 tail call

--- rgn_scala_s99/src/main/scala/org/rgn/scala/s99/lists/P04.scala
+++ rgn_scala_s99/src/main/scala/org/rgn/scala/s99/lists/P04.scala
@@ -1,6 +1,7 @@
 package org.rgn.scala.s99.lists
 
 import org.scalatest.matchers.ShouldMatchers
+import scala.annotation.tailrec
 
 /**
  * S-99: Ninety-Nine Scala Problems
@@ -20,12 +21,35 @@ object P04 extends ShouldMatchers {
     case Nil => 0
   }
 
+  /**
+   * http://blog.richdougherty.com/2009/04/tail-calls-tailrec-and-trampolines.html
+   * 
+   * @param list
+   * @return
+   */
+  def lengthTailRec(list: List[Int]): Int = {
+    @tailrec
+    def lengthTR(i: Int, ls: List[Int]): Int = ls match {
+      case Nil => i
+      case _ :: tail => lengthTR(i + 1, tail)
+    }
+
+    lengthTR(0, list)
+  }
+
   def main(args: Array[String]): Unit = {
     val l = List(1, 1, 2, 3, 5, 8)
 
     val res = length(l)
 
     res should be(6)
+
     println(res)
+
+    val resTR = lengthTailRec(l)
+
+    resTR should be(6)
+    println(resTR)
+
   }
 }
